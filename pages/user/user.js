aa7b262054d3c0de32d56a5c6fc64ab6f995d572
mutun <tun224813@gmail.com>
2019-04-26 11:54:31 +0800

    个人中心完成

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,58 +1,33 @@
 // pages/user/user.js
 Page({
   data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    count:0
   },
-  onLoad() {
-    // 查看是否授权
-    wx.getSetting({
-      success(res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success(res) {
-              console.log(res.userInfo)
-              wx: wx.request({
-                url: 'http://localhost:8080/user/login',
-                data: {
-                  code: res.code,
-                  userInfo: res.userInfo
-                },
-                method: 'POST',
-                success: function (res) {
-                  console.log(res);
-                },
-                fail: function (res) { },
-              })
-            }
-          })
-        }
-      }
-    })
-  },
+
   /**
    * 登陆按钮
    */
   bindGetUserInfo(e) {
-    console.log(e.detail.userInfo)
-    wx: wx.request({
-      url: 'http://localhost:8080/user/login',
-      data: {
-        userInfo: e.detail.userInfo
-      },
-      method: 'POST',
-      success: function (res) {
-        console.log(res);
-      },
-      fail: function (res) { },
-    })
   },
   
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var that = this
+    wx.request({
+      url: 'http://localhost:8080/question/getStatistic',
+      method: 'POST',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        console.log(res.data.data[0].data)
+        that.setData({ count: res.data.data[0].data })
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
 
   /**
@@ -73,7 +48,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
